d41e5fe1c69d854eca3ca8f1e0897ea2900df3c2
GuiOliveiraz <guilhermeoliveirayear4@gmail.com>
2024-06-19 22:02:21 -0300

    clickbotton

---
+++ src/test/java/br/com/chronosacademy/pages/CursoPage.java
@@ -0,0 +1,6 @@
+package br.com.chronosacademy.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CursoPage {
+}
---
+++ src/test/java/br/com/chronosacademy/pages/PrincipalPage.java
@@ -0,0 +1,12 @@
+package br.com.chronosacademy.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class PrincipalPage {
+    WebDriver driver;
+
+    public PrincipalPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+}
